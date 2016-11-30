03b281f5b0ecbbb269f8109eadb613140502e169
sanghee lee <louder81@gmail.com>
2016-11-30 19:49:26 +0900

    start for xml test

---
+++ xmltest.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <libxml2/libxml/encoding.h>
+#include <libxml2/libxml/xmlwriter.h>
+
+using namespace std;
+
+class XMLGenerator
+{
+
+};
+
+int main()
+{
+	XMLGenerator xmlGenerator;
+	cout << "Hello world!" << endl;
+}
