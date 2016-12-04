ba6ba5c922da512e16faf4f24877bd689844635b
sanghee lee <louder81@gmail.com>
2016-12-05 03:00:27 +0900

    applying decorator pattern on xml generation

--- xmlGenerator.cpp
+++ xmlGenerator.cpp
@@ -3,25 +3,227 @@
 
 using namespace std;
 
+namespace xmlGenerator
+{
+
+void BaseXMLGenerator::setConditions(conditions condi)
+{
+
+}
+
+conditions BaseXMLGenerator::getConditions()
+{
+	return condition;
+}
+
+void BaseXMLGenerator::setXMLData(vector<pair<string, string> > meta, vector<string> data)
+{
+	cout << "BaseXMlGenerator setXMLData!" << endl;
+
+}
+
+string BaseXMLGenerator::generateXMLDocument()
+{
+	return "aaa";
+}
+
+void XMLResult::setConditions(conditions condi)
+{
+//	conditions = condi;
+}
+
+conditions XMLResult::getConditions()
+{
+	return xmlGenerator->condition;
+}
+
+void XMLResult::setXMLData(vector<pair<string, string> > meta, vector<string> data)
+{
+	cout << "XMLResult setXMLData!" << endl;
+
+	// traverse for upper class
+	xmlGenerator->setXMLData(meta, data);
+}
+
+string XMLResult::generateXMLDocument()
+{
+	cout << "XMLResult generateXMLDocument!" << endl;
+
+	// print out for upper class
+	xmlDocument = xmlGenerator->generateXMLDocument();
+	return xmlDocument;
+}
+
+void XMLSchema::setConditions(conditions condi)
+{
+//	conditions = condi;
+}
+
+conditions XMLSchema::getConditions()
+{
+	return condition;
+}
+
+void XMLSchema::setXMLData(vector<pair<string, string> > meta, vector<string> data)
+{
+	cout << "XMLSchema setXMLData!" << endl;
+
+	// traverse for upper class
+	XMLResult::setXMLData(meta, data);
+
+	// traverse for this class
+	vector<string>::iterator itr;
+
+	int count = 0;
+	for (itr = data.begin(); itr != data.end(); ++itr)
+	{
+		cout << ++count << " " << *itr << endl;
+		xmlDocument += *itr;
+		xmlDocument += "\n";
+	}
+}
+
+string XMLSchema::generateXMLDocument()
+{
+	cout << "XMLSchema generateXMLDocument!" << endl;
+
+	// print out for upper class
+	XMLResult::generateXMLDocument();
+
+	// print out for this class
+	cout << xmlDocument << endl;
+
+	return xmlDocument;
+}
+
+void XMLDTD::setConditions(conditions condi)
+{
+//	conditions = condi;
+}
+
+conditions XMLDTD::getConditions()
+{
+	return condition;
+}
+
+void XMLDTD::setXMLData(vector<pair<string, string> > meta, vector<string> data)
+{
+	cout << "XMLDTD setXMLData!" << endl;
+
+	// traverse for upper class
+	XMLResult::setXMLData(meta, data);
+
+	// traverse for this class
+	vector<string>::iterator itr;
+
+	int count = 0;
+	for (itr = data.begin(); itr != data.end(); ++itr)
+	{
+		cout << ++count << " " << *itr << endl;
+		xmlDocument += *itr;
+		xmlDocument += "\n";
+	}
+}
+
+string XMLDTD::generateXMLDocument()
+{
+	cout << "XMLDTD generateXMLDocument!" << endl;
+	// print out for upper class
+	XMLResult::generateXMLDocument();
+
+	// print out for this class
+	cout << xmlDocument << endl;
+
+	return xmlDocument;
+}
+
+void XMLDocument::setConditions(conditions condi)
+{
+//	conditions = condi;
+}
+
+conditions XMLDocument::getConditions()
+{
+	return condition;
+}
+
+void XMLDocument::setXMLData(vector<pair<string, string> > meta, vector<string> data)
+{
+	cout << "XMLDocument setXMLData!" << endl;
+
+	// traverse for upper class
+	XMLResult::setXMLData(meta, data);
+
+	// traverse for this class
+	vector<string>::iterator itr;
+
+	for (itr = data.begin(); itr != data.end(); ++itr)
+	{
+		xmlDocument += *itr;
+		xmlDocument += "\n";
+	}
+}
+
+string XMLDocument::generateXMLDocument()
+{
+	cout << "XMLDocument generateXMLDocument!" << endl;
+
+	// print out for upper class
+	XMLResult::generateXMLDocument();
+
+	// print out for this class
+	cout << xmlDocument << endl;
+
+	return xmlDocument;
+}
+
+
+}
+
 int main()
 {
-	xmlGenerator::XMLGenerator xmlGenerator;
+//	xmlGenerator::XMLGenerator xmlGenerator;
 
-	xmlNodePtr n;
-	xmlDocPtr doc;
-	xmlChar *xmlBuf;
-	int bufSize;
+	xmlGenerator::XMLGenerator* baseXMLGenerator =
+		new xmlGenerator::XMLSchema(
+//		new xmlGenerator::XMLDTD(
+		new xmlGenerator::XMLDocument(
+		new xmlGenerator::BaseXMLGenerator());
 
-	doc = xmlNewDoc(BAD_CAST "1.0");
-	n = xmlNewNode(NULL, BAD_CAST "root");
-	xmlNodeSetContent(n, BAD_CAST "content");
-	xmlDocSetRootElement(doc, n);
+	std::vector<std::pair<std::string, std::string> > meta;
+	meta.push_back(std::make_pair("C1", "integer"));
+	meta.push_back(std::make_pair("C2", "string"));
 
-	xmlDocDumpFormatMemory(doc, &xmlBuf, &bufSize, 1);
-	cout << xmlBuf << endl;
+	std::vector<string> data;
 
-	xmlFree(xmlBuf);
-	xmlFreeDoc(doc);
+	data.push_back("a");
+	data.push_back("b");
+	data.push_back("c");
+	data.push_back("d");
+	data.push_back("e");
+
+	baseXMLGenerator->setXMLData(meta, data);
+	//std::cout << baseXMLGenerator->generateXMLDocument() << endl;
+
+	delete baseXMLGenerator;
+
+//	xmlNodePtr n;
+//	xmlDocPtr doc;
+//	xmlChar *xmlBuf;
+//	int bufSize;
+//
+//	doc = xmlNewDoc(BAD_CAST "1.0");
+//	n = xmlNewNode(NULL, BAD_CAST "root");
+//	xmlNodeSetContent(n, BAD_CAST "content");
+//	xmlDocSetRootElement(doc, n);
+//
+//	xmlDocDumpFormatMemory(doc, &xmlBuf, &bufSize, 1);
+//	cout << xmlBuf << endl;
+//
+//	xmlFree(xmlBuf);
+//	xmlFreeDoc(doc);
+//
+
 
 	return 1;
 }
--- xmlGenerator.h
+++ xmlGenerator.h
@@ -1,22 +1,114 @@
 //#include <libxml/encoding.h>
 #include <libxml/xmlwriter.h>
 #include <libxml/parser.h>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 namespace xmlGenerator
 {
 
+struct conditions
+{
+	bool isHeader;
+	bool isFooter;
+};
+
+typedef struct conditions conditions;
+
 class XMLGenerator
 {
-	struct conditions
-	{
-		bool isHeader;
-		bool isFooter;
-	};
+public:
+	conditions condition;
+	string xmlDocument;
 
-	typedef struct conditions conditions;
+	virtual void setConditions(conditions condi) = 0;
+	virtual conditions getConditions() = 0;
+
+	virtual void setXMLData(vector<pair<string, string> > meta, vector<string> data) = 0;
+	virtual string generateXMLDocument() = 0;
+//	virtual bool verifyXML() = 0;
+};
+
+class BaseXMLGenerator : public XMLGenerator
+{
+	void setConditions(conditions condi);
+	conditions getConditions();
+
+	void setXMLData(vector<pair<string, string> > meta, vector<string> data);
+	string generateXMLDocument();
+};
+
+class XMLResult : public XMLGenerator
+{
+private:
+	XMLGenerator* xmlGenerator;
+
+public:
+	XMLResult(XMLGenerator* xmlGenerator)
+	{
+		this->xmlGenerator = xmlGenerator;
+	}
+
+	~XMLResult()
+	{
+		if (!xmlGenerator) delete xmlGenerator;
+	}
 
 	void setConditions(conditions condi);
 	conditions getConditions();
+
+	void setXMLData(vector<pair<string, string> > meta, vector<string> data);
+	string generateXMLDocument();
 };
+
+class XMLSchema : public XMLResult
+{
+public:
+	XMLSchema(XMLGenerator* xmlGenerator) : XMLResult(xmlGenerator) { }
+
+	void setConditions(conditions condi);
+	conditions getConditions();
+
+	void setXMLData(vector<pair<string, string> > meta, vector<string> data);
+	string generateXMLDocument();
+};
+
+class XMLDTD : public XMLResult
+{
+public:
+	XMLDTD(XMLGenerator* xmlGenerator) : XMLResult(xmlGenerator) { }
+
+	void setConditions(conditions condi);
+	conditions getConditions();
+
+	void setXMLData(vector<pair<string, string> > meta, vector<string> data);
+	string generateXMLDocument();
+};
+
+class XMLDocument : public XMLResult
+{
+public:
+	XMLDocument(XMLGenerator* xmlGenerator) : XMLResult(xmlGenerator) { }
+
+	void setConditions(conditions condi);
+	conditions getConditions();
+
+	void setXMLData(vector<pair<string, string> > meta, vector<string> data);
+	string generateXMLDocument();
+};
+
+
+//class XMLDTD : public XMLResult
+//{
+//	XMLDTD(XMLGenerator* xmlGenerator) : XMLResult(XMLGenerator* xmlGenerator) { }
+//};
+////
+//class XMLDocument : public XMLResult
+//{
+//	XMLDocument(XMLGenerator* xmlGenerator) : XMLResult(XMLGenerator* xmlGenerator) { }
+//};
+//
 
 }
